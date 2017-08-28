5c6b06fc5501d664d232137fde1c3341797d6364
dparks1134 <donovan.parks@gmail.com>
2017-08-29 07:30:24 +1000

    Updated align and classify code to automatically consider domain of each User genome.

--- gtdbtk/config/config.py
+++ gtdbtk/config/config.py
@@ -46,11 +46,15 @@ GENERIC_PATH = '/srv/db/gtdbtk/'
 TIGRFAM_HMM_DIR = GENERIC_PATH + 'markers/pfam/individual_hmms/'
 PFAM_HMM_DIR = GENERIC_PATH + 'markers/tigrfam/individual_hmms/'
 
-# Path to MSA and Taxonomy
+# Information for aligning genomes
+DEFAULT_DOMAIN_THRESHOLD = 10.0
+AR_MARKER_COUNT = 122
+BAC_MARKER_COUNT = 120
 
+# Path to MSA and Taxonomy
 MSA_FOLDER = GENERIC_PATH + "msa/"
 CONCAT_BAC120 = MSA_FOLDER + "gtdb_r78_bac120.faa"
-CONCAT_AR122 = MSA_FOLDER + "gtdb_concatenated_archaea.faa"
+CONCAT_AR122 = MSA_FOLDER + "gtdb_r78_ar122.faa"
 CONCAT_RPS23 = MSA_FOLDER + "gtdb_r78_rps23.faa"
 
 TAX_FOLDER = GENERIC_PATH + "taxonomy/"
@@ -64,8 +68,6 @@ MASK_RPS23 = "gtdb_r78_rps23.mask"
 
 # Path to pplacer data
 PPLACER_DIR = GENERIC_PATH + "pplacer/pruned_trees/"
-PPLACER_OUT = "pplacer.out"
-PPLACER_JSON_OUT = "pplacer.json"
 
 PPLACER_BAC120_REF_PKG = "gtdb_r78_bac120.refpkg"
 PPLACER_AR122_REF_PKG = "gtdb_r78_ar122.refpkg"
