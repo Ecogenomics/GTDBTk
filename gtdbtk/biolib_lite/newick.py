0fb6b5c7d41c0d78e0d03aa43c941d772d4b0f9e
Aaron Mussig <aaronmussig@gmail.com>
2019-02-14 16:26:49 +1000

    Added missing biolib_lite dependency.

---
+++ gtdbtk/biolib_lite/newick.py
@@ -0,0 +1,61 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+
+from common import is_float
+
+
+'''Helper functions for parsing Newick information.'''
+
+
+def parse_label(label):
+    """Parse a Newick label which may contain a support value, taxon, and/or auxiliary information.
+
+    Parameters
+    ----------
+    label : str
+        Internal label in a Newick tree.
+
+    Returns
+    -------
+    float
+        Support value specified by label, or None
+    str
+        Taxon specified by label, or None
+    str
+        Auxiliary information, on None
+    """
+
+    support = None
+    taxon = None
+    auxiliary_info = None
+
+    if label:
+        label = label.strip()
+        if '|' in label:
+            label, auxiliary_info = label.split('|')
+
+        if ':' in label:
+            support, taxon = label.split(':')
+            support = float(support)
+        else:
+            if is_float(label):
+                support = float(label)
+            elif label != '':
+                taxon = label
+
+    return support, taxon, auxiliary_info
