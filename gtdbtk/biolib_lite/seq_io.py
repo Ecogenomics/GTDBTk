608c691da7b096a518caeae73e7df9a4bc18e5ee
Aaron Mussig <aaronmussig@gmail.com>
2019-06-05 10:19:33 +1000

    Support for gzipped genomes and unit tests.

--- gtdbtk/biolib_lite/seq_io.py
+++ gtdbtk/biolib_lite/seq_io.py
@@ -207,3 +207,21 @@ def read_seq(seq_file, keep_annotation=False):
     else:
         for rtn in read_fasta_seq(seq_file, keep_annotation):
             yield rtn
+
+def write_fasta(seqs, fasta_file, wrap=80):
+    """Write sequences to a fasta file.
+
+    Parameters
+    ----------
+    dict : dict[seq_id] -> seq
+        Sequences indexed by sequence id.
+    fasta_file : str
+        Path to write the sequences to.
+    wrap: int
+        Number of AA/NT before the line is wrapped.
+    """
+    with open(fasta_file, 'w') as f:
+        for gid, gseq in seqs.items():
+            f.write('>{}\n'.format(gid))
+            for i in range(0, len(gseq), wrap):
+                f.write('{}\n'.format(gseq[i:i+wrap]))
