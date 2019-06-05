Masking unit tests, corrected numpy masking

gtdbtk/markers.py
@@ -33,7 +33,7 @@ from external.hmm_aligner import HmmAligner
 
 import config.config as Config
 from gtdbtk.config.output import *
-from gtdbtk.exceptions import GenomeMarkerSetUnknown
+from gtdbtk.exceptions import GenomeMarkerSetUnknown, MSAMaskLengthMismatch
 
 from tools import merge_two_dicts
 
@@ -317,11 +317,11 @@ class Markers(object):
         aligned_genomes = merge_two_dicts(gtdb_msa, user_msa)
 
         mask = open(msa_mask).readline().strip()
-        list_mask = [True if c == '1' else False for c in mask]
+        list_mask = np.array([True if c == '1' else False for c in mask], dtype=bool)
 
         if len(mask) != len(aligned_genomes.values()[0]):
             self.logger.error('Mask and alignment length do not match.')
-            sys.exit()
+            raise MSAMaskLengthMismatch
 
         output_seqs = {}
         pruned_seqs = {}