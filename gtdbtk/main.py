c3caf87ef0ef0fa404f32bbe9a6a3a312121bdf0
pchaumeil <p.chaumeil@gmail.com>
2021-03-26 15:06:01 +1000

    CLI issue when using scratch_dir in classify_wf (issue #311) .

--- gtdbtk/main.py
+++ gtdbtk/main.py
@@ -712,8 +712,6 @@ class OptionsParser(object):
             options.max_consensus = None
             options.rnd_seed = None
             options.skip_trimming = False
-            options.scratch_dir = None
-            options.recalculate_red = False
 
             self.align(options)
 
