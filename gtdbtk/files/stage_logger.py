71035cff028962ce80b563131802ca274ca68131
pchaumeil <uqpchaum@uq.edu.au>
2025-10-28 13:45:17 +0100

    refactor(stage_logger): issue #669 , migrate to dataclass for step definitions, remove pydantic reference. Should solve conflict in python 3.14

--- gtdbtk/files/stage_logger.py
+++ gtdbtk/files/stage_logger.py
@@ -1,208 +1,187 @@
 import json
-import os
-from typing import List, Optional, Dict
-
-from pydantic import BaseModel
+from dataclasses import dataclass, field, asdict
+from typing import List, Optional, Dict, ClassVar
 
 
-class Steps(BaseModel):
-    """ Information about a specific step of GTDB-Tk"""
+@dataclass
+class Steps:
+    """Information about a specific step of GTDB-Tk"""
     name: str
-    output_dir: Optional[str]
-    starts_at: Optional[str]
-    ends_at: Optional[str]
-    duration: Optional[str]
-    status: Optional[str]
+    output_dir: Optional[str] = None
+    starts_at: Optional[str] = None
+    ends_at: Optional[str] = None
+    duration: Optional[str] = None
+    status: Optional[str] = None
 
     def is_complete(self) -> bool:
-        if self.status == "completed":
-            return True
-        else:
-            return False
+        return self.status == "completed"
 
+
+@dataclass
 class ANIScreenStep(Steps):
-    """ Information about the ANI screen step of GTDB-Tk"""
-    name: str = "ANI screen"
-    genome_dir: Optional[str]
-    batchfile: Optional[str]
-    min_af: Optional[str]
-    output_files: Optional[Dict]
+    """Information about the ANI screen step of GTDB-Tk"""
+    name: str = field(default="ANI screen", init=False)
+    genome_dir: Optional[str] = None
+    batchfile: Optional[str] = None
+    min_af: Optional[str] = None
+    output_files: Optional[Dict] = None
 
+
+@dataclass
 class IdentifyStep(Steps):
-    """ Information about the Identify step of GTDB-Tk"""
-    name: str = "identify"
-    genes: Optional[bool]
-    extension: Optional[str]
-    write_single_copy_genes: Optional[bool]
-    genome_dir: Optional[str]
-    batchfile: Optional[str]
-    output_files: Optional[Dict]
+    """Information about the Identify step of GTDB-Tk"""
+    name: str = field(default="identify", init=False)
+    genes: Optional[bool] = None
+    extension: Optional[str] = None
+    write_single_copy_genes: Optional[bool] = None
+    genome_dir: Optional[str] = None
+    batchfile: Optional[str] = None
+    output_files: Optional[Dict] = None
 
+
+@dataclass
 class AlignStep(Steps):
-    """ Information about the Align step of GTDB-Tk"""
-    name: str = "align"
-    identify_dir: Optional[str]
-    skip_gtdb_refs: Optional[bool]
-    taxa_filter: Optional[str]
-    min_perc_aa: Optional[str]
-    custom_msa_filters: Optional[bool]
-    skip_trimming: Optional[bool]
-    rnd_seed: Optional[str]
-    cols_per_gene: Optional[str]
-    min_consensus: Optional[str]
-    max_consensus: Optional[str]
-    min_perc_taxa: Optional[str]
-    outgroup_taxon: Optional[str]
-    output_files: Optional[Dict]
+    """Information about the Align step of GTDB-Tk"""
+    name: str = field(default="align", init=False)
+    identify_dir: Optional[str] = None
+    skip_gtdb_refs: Optional[bool] = None
+    taxa_filter: Optional[str] = None
+    min_perc_aa: Optional[str] = None
+    custom_msa_filters: Optional[bool] = None
+    skip_trimming: Optional[bool] = None
+    rnd_seed: Optional[str] = None
+    cols_per_gene: Optional[str] = None
+    min_consensus: Optional[str] = None
+    max_consensus: Optional[str] = None
+    min_perc_taxa: Optional[str] = None
+    outgroup_taxon: Optional[str] = None
+    output_files: Optional[Dict] = None
 
+
+@dataclass
 class ClassifyStep(Steps):
-    """ Information about the Classify step of GTDB-Tk"""
-    name: str = "classify"
-    align_dir: Optional[str]
-    genome_dir: Optional[str]
-    batchfile: Optional[str]
-    scratch_dir: Optional[str]
-    debug_option: Optional[bool]
-    full_tree: Optional[bool]
-    skip_ani_screen: Optional[bool]
-    output_files: Optional[Dict]
+    """Information about the Classify step of GTDB-Tk"""
+    name: str = field(default="classify", init=False)
+    align_dir: Optional[str] = None
+    genome_dir: Optional[str] = None
+    batchfile: Optional[str] = None
+    scratch_dir: Optional[str] = None
+    debug_option: Optional[bool] = None
+    full_tree: Optional[bool] = None
+    skip_ani_screen: Optional[bool] = None
+    output_files: Optional[Dict] = None
 
+
+@dataclass
 class InferStep(Steps):
-    """ Information about the Infer step of GTDB-Tk"""
-    name: str = "infer"
-    msa_file: Optional[str]
-    prot_model: Optional[str]
-    no_support: Optional[bool]
-    gamma: Optional[bool]
+    """Information about the Infer step of GTDB-Tk"""
+    name: str = field(default="infer", init=False)
+    msa_file: Optional[str] = None
+    prot_model: Optional[str] = None
+    no_support: Optional[bool] = None
+    gamma: Optional[bool] = None
 
+
+@dataclass
 class RootStep(Steps):
-    """ Information about the Root step of GTDB-Tk"""
-    name: str = "root"
-    input_tree: Optional[str]
-    gtdbtk_classification_file: Optional[str]
-    custom_taxonomy_file: Optional[str]
-    outgroup_taxon: Optional[str]
+    """Information about the Root step of GTDB-Tk"""
+    name: str = field(default="root", init=False)
+    input_tree: Optional[str] = None
+    gtdbtk_classification_file: Optional[str] = None
+    custom_taxonomy_file: Optional[str] = None
+    outgroup_taxon: Optional[str] = None
 
+
+@dataclass
 class DecorateStep(Steps):
-    """ Information about the Decorate step of GTDB-Tk"""
-    name: str = "decorate"
-    input_tree: Optional[str]
-    gtdbtk_classification_file: Optional[str]
-    custom_taxonomy_file: Optional[str]
-    suffix: Optional[str]
-    output_files: Optional[Dict]
+    """Information about the Decorate step of GTDB-Tk"""
+    name: str = field(default="decorate", init=False)
+    input_tree: Optional[str] = None
+    gtdbtk_classification_file: Optional[str] = None
+    custom_taxonomy_file: Optional[str] = None
+    suffix: Optional[str] = None
+    output_files: Optional[Dict] = None
 
 
+@dataclass
+class _StageLoggerImpl:
+    """Internal implementation of StageLogger"""
+    version: Optional[str] = None
+    command_line: Optional[str] = None
+    database_version: Optional[str] = None
+    database_path: Optional[str] = None
+    steps: List[Steps] = field(default_factory=list)
+    output_dir: Optional[str] = None
+    path: Optional[str] = None
 
-class StageLogger(object):
-    class __StageLogger(BaseModel):
+    def __str__(self):
+        return repr(self)
 
-        version: Optional[str]
-        command_line: Optional[str]
-        database_version: Optional[str]
-        database_path: Optional[str]
-        steps: List[Steps]
-        output_dir: Optional[str]
-        path: Optional[str]
+    def write(self):
+        if not self.path:
+            raise ValueError("Path not set for StageLogger")
+        with open(self.path, "w") as f:
+            json.dump(asdict(self), f, indent=4)
 
-        def __str__(self):
-            return repr(self) + self.val
+    def has_stage(self, stage_class: type) -> bool:
+        return any(isinstance(x, stage_class) for x in self.steps)
 
-        def write(self):
-            with open(self.path, "w") as f:
-                f.write(self.json(indent=4))
+    def get_stage(self, stage_class: type) -> Optional[Steps]:
+        for x in self.steps:
+            if isinstance(x, stage_class):
+                return x
+        return None
 
-        def has_stage(self, stage_object: object) -> bool:
-            processed_steps = [x for x in self.steps if isinstance(x, stage_object)]
-            if len(processed_steps) > 0:
-                return True
-            else:
-                return False
+    def reset_steps(self, keep_steps: Optional[List[str]] = None):
+        if keep_steps is None:
+            self.steps = []
+        else:
+            self.steps = [x for x in self.steps if x.name in keep_steps]
 
-        def get_stage(self, stage_object: object) -> object:
-            processed_steps = [x for x in self.steps if isinstance(x, stage_object)]
-            if len(processed_steps) > 0:
-                return processed_steps[0]
-            else:
-                return None
+    def read_existing_steps(self):
+        if not self.path:
+            raise ValueError("Path not set for StageLogger")
 
-        # create reset steps function, which takes an optional list of steps to keep
-        # if no list is provided, all steps are removed
+        with open(self.path, "r") as f:
+            data = json.load(f)
+            steps = []
+            for step in data.get('steps', []):
+                name = step.get('name')
+                if name == "ANI screen":
+                    step_object = ANIScreenStep(**step)
+                elif name == "identify":
+                    step_object = IdentifyStep(**step)
+                elif name == "align":
+                    step_object = AlignStep(**step)
+                elif name == "classify":
+                    step_object = ClassifyStep(**step)
+                elif name == "infer":
+                    step_object = InferStep(**step)
+                elif name == "root":
+                    step_object = RootStep(**step)
+                elif name == "decorate":
+                    step_object = DecorateStep(**step)
+                else:
+                    raise Exception(f"Unknown step name {name}")
+                steps.append(step_object)
+            self.steps = steps
 
-        def reset_steps(self, keep_steps: List[str] = None):
-            if keep_steps is None:
-                self.steps = []
-            else:
-                self.steps = [x for x in self.steps if x.name in keep_steps]
 
-        def read_existing_steps(self):
-            with open(self.path, "r") as f:
-                data = json.load(f)
-                #stage_logger = self.parse_obj(data)
-                steps = []
-                for step in data.get('steps'):
-                    if step.get('name') == "ANI screen":
-                        step_object = ANIScreenStep(**step)
-                    elif step.get('name') == "identify":
-                        step_object = IdentifyStep(**step)
-                    elif step.get('name') == "align":
-                        step_object = AlignStep(**step)
-                    elif step.get('name') == "classify":
-                        step_object = ClassifyStep(**step)
-                    elif step.get('name') == "infer":
-                        step_object = InferStep(**step)
-                    else:
-                        raise Exception(f"Unknown step name {step.name}")
-                    steps.append(step_object)
-                self.steps = steps
+class StageLogger:
+    """Singleton wrapper for StageLogger"""
+    _instance = None
 
-    instance = None
-    def __new__(cls): # __new__ always a classmethod
-        if not StageLogger.instance:
-            StageLogger.instance = StageLogger.__StageLogger(steps=[])
-        return StageLogger.instance
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            cls._instance._impl = _StageLoggerImpl(steps=[])
+        return cls._instance
+
     def __getattr__(self, name):
-        return getattr(self.instance, name)
-    def __setattr__(self, name):
-        return setattr(self.instance, name)
+        return getattr(self._impl, name)
 
-
-
-
-
-# class StageLoggerFile:
-#     def __init__(self,output_dir: str):
-#         self.output_dir = output_dir
-#
-#     # We generate the StageLogger object
-#     def setStageLogger(self, version:str, command_line: str,
-#                  database_version: str, database_path: str):
-#         self.path = os.path.join(self.output_dir, "gtdbtk.json")
-#         self.stage_logger = StageLogger(version = version,
-#                                         command_line=command_line,
-#                                         database_version=database_version,
-#                                         database_path=database_path,
-#                                         steps=[])
-#     def write(self):
-#         with open(self.path, "w") as f:
-#             f.write(self.stage_logger.json(indent=4))
-#
-#     def read(self):
-#         with open(self.path, "r") as f:
-#             self.stage_logger = StageLogger.parse_raw(f.read())
-#             steps = []
-#             for step in self.stage_logger.steps:
-#                 if step.name == "ANI screen":
-#                     step_object = ANIScreenStep(**step.dict())
-#                 elif step.name == "identify":
-#                     step_object = IdentifyStep(**step.dict())
-#                 elif step.name == "align":
-#                     step_object = AlignStep(**step.dict())
-#                 elif step.name == "classify":
-#                     step_object = ClassifyStep(**step.dict())
-#                 elif step.name == "infer":
-#                     step_object = InferStep(**step.dict())
-#                 else:
-#                     raise Exception(f"Unknown step name {step.name}")
-#                 steps.append(step_object)
-#             self.stage_logger.steps = steps
+    def __setattr__(self, name, value):
+        if name == '_impl':
+            super().__setattr__(name, value)
+        else:
+            setattr(self._impl, name, value)
