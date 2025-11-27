4c9ed9c88641516b77113814ffab6d1a70865224
pchaumeil <uqpchaum@uq.edu.au>
2025-11-27 12:14:21 +0100

    feat(modify code for Mash 0.3):
    add output_dir parameter to run_skani and run_vs_all_reps functions
    change dist comment to sketch and search to speed up the process

--- gtdbtk/config/output.py
+++ gtdbtk/config/output.py
@@ -63,6 +63,9 @@ PATH_AR53_PRESCREEN_MSA = join(DIR_CLASSIFY_INTERMEDIATE, '{prefix}.ar53.prescre
 #Command: ani_screen
 DIR_ANISCREEN = join(DIR_CLASSIFY, 'ani_screen')
 DIR_ANISCREEN_INTERMEDIATE = join(DIR_ANISCREEN, 'intermediate_results')
+DIR_ANISCREEN_SKETCH_SKANI = join(DIR_ANISCREEN_INTERMEDIATE, 'sketch_skani')
+DIR_ANISCREEN_SKETCH_REF = join(DIR_ANISCREEN_SKETCH_SKANI, 'sketch_ref')
+DIR_ANISCREEN_SKETCH_QRY = join(DIR_ANISCREEN_SKETCH_SKANI, 'sketch_query')
 PATH_BAC120_ANI_SKANI = join(DIR_ANISCREEN, '{prefix}.bac120_skani.summary.tsv')
 PATH_AR53_ANI_SKANI = join(DIR_ANISCREEN, '{prefix}.ar53_skani.summary.tsv')
 
